8314eba034da8b0f7c78c14d7c6a215deee88053
Wangzhe <xp406181@163.com>
2025-08-06 18:02:18 +0800

    增加一个Procedure专用代码集来存放所有的procedure

--- Scripts/Editor/Misc/Type.cs
+++ Scripts/Editor/Misc/Type.cs
@@ -22,6 +22,7 @@ namespace UnityGameFramework.Editor
             "UnityGameFramework.Runtime",
 #endif
             "Assembly-CSharp",
+            "Procedure",
         };
 
         private static readonly string[] RuntimeOrEditorAssemblyNames =
